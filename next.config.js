97deb92dd7e36e110e783818934e87657ad5564d
Nathanael Wa Mwenze <11463376+nmwenz90@users.noreply.github.com>
2020-08-11 01:25:25 -0700

    created env files updated ui

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    env: {
+        HOST: process.env.HOST,
+        DATABASE: process.env.DATABASE,
+        USER: process.env.ADMIN,
+        PASSWORD: process.env.PASSWORD,
+    }
+}
