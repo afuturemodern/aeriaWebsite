09479dfac825b4675f6c683e4958258f2e01105c
Nathanael Wa Mwenze <11463376+nmwenz90@users.noreply.github.com>
2020-08-11 02:55:53 -0700

    removed console log from database endpoint

--- pages/api/database.js
+++ pages/api/database.js
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import mysql from 'serverless-mysql'
 // USING MYSQL
 //declare mysql db
-console.log('env files', process.env.HOST, process.env.DATABASE, process.env.DB_USERNAME, process.env.PASSWORD)
+
 const db = mysql({
     config: {
         host: process.env.HOST,
